4055f6a10fc0a0a9dad5969cffe5154241e56e2a
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-30 18:32:50 -0300

    Section 33.434 - Styling With Tailwind CSS

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,7 @@
-import Counter from "@/app/_components/Counter";
-
 export default function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-      <Counter users={data} />
     </div>
   );
 }
