54752a82936c215ea0a995c313a1f86032547584
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-30 08:43:16 -0300

    Section 32.426 - Fetching Data in a Page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,17 @@
-import Navigation from "../components/Navigation";
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
 
-export default function Page() {
+  console.log(data);
+
   return (
     <div>
       <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
