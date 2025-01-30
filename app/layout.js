9a0fd47839d10c5c3f1cfe58cb29992cd2d92b5d
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-30 20:17:18 -0300

    Section 33.437 - Improving the Navigation and Root Layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import Header from "./_components/Header";
 
 import { Josefin_Sans } from "next/font/google";
 
@@ -26,14 +27,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
