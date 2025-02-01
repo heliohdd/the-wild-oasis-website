199f18e32ad2c5c67d682563c03895b22317e201
Helio Dourado <heliohdd@yahoo.com.br>
2025-02-01 12:57:24 -0300

    Section 34.456 - Static Site Generation (SSG)

--- app/account/page.js
+++ app/account/page.js
@@ -5,7 +5,7 @@ export const metadata = {
 export default function Page() {
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome Érica
+      Welcome Hélio
     </h2>
   );
 }
