56b6090fa6a8d5efed848256d94a5f9088beef5f
Helio Dourado <heliohdd@yahoo.com.br>
2025-02-27 18:44:11 -0300

    Section 37.484 - Deleting a Reservation

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -221,11 +221,11 @@ export async function createBooking(newBooking) {
 // DELETE
 
 // export async function deleteBooking(id) {
-//   const { data, error } = await supabase.from("bookings").delete().eq("id", id);
+// const { data, error } = await supabase.from("bookings").delete().eq("id", id);
 
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Booking could not be deleted");
-//   }
+// if (error) {
+//   console.error(error);
+//   throw new Error("Booking could not be deleted");
+// }
 //   return data;
 // }
